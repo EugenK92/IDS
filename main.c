b43fa24a4b92d6379d596d5ca6b6255346aee942
Eugen <e.k.92@gmx.de>
2018-03-22 12:23:07 +0100

    Manual

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 //include external libraries
 #include <stdio.h>
+#include <string.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <sqlite3.h>
@@ -12,19 +13,71 @@
 
 /* sudo apt-get install libsqlite3-dev */
 
-int main (int argc, char** argv)
-{
-    
-    create_table();
-   // chdir("/etc");
-   if (argc < 2) {
-       printf("usage: ./IDS <starting path>\n");
-   }
-   else {
-       scan_dir(argv[1]);
-   }
+int check_parameter(int len, char* argv[], char* param);
+void print_manual();
+
+int main (int argc, char* argv[]) {
+
+    if (argc < 2) {
+        print_manual();
+    } 
+    else {
+        if (check_parameter(argc, argv, "--init") != 0 || check_parameter(argc, argv, "-i") != 0) {
+            printf("Start init\n");
+        }
+        else if (check_parameter(argc, argv, "--check") != 0 || check_parameter(argc, argv, "-c") != 0) {
+            printf("Start check\n");
+        }
+        else if (check_parameter(argc, argv, "--help") != 0 || check_parameter(argc, argv, "-h") != 0) {
+            print_manual();
+        }
+        else {
+            print_manual();
+        }
+    }   
+//     create_table();
+//    // chdir("/etc");
+//    if (argc < 2) {
+//        printf("usage: ./IDS <starting path>\n");
+//    }
+//    else {
+//        scan_dir(argv[1]);
+//    }
     // printf("\n");
     // printf("%s\n", select_all_data());
     
 }
+
+int check_parameter(int len, char* argv[], char* param) {
+    int end = false;
+    int index = 0;
+    int result = 0;
+    while (!end && index < len - 1) {
+        index++;
+        if (strcmp(argv[index], param) == 0) {
+            result = index;
+            end = true;
+        }
+    }
+    return result;
+}
+
+void print_manual() {
+    printf("Aufruf: ./IDS [OPTION]... [PFAD]...\n");
+    printf("Scannen des angegebenen PFADes. Standard ist das root '/' Verzeichnis.\n");
+    printf("Erforderliche Argumente für lange Optionen sind auch für kurze erforderlich.\n");
+    printf("\t-c, --check\t\tFühre das Programm für ein Verzeichnis aus. Standard ist das root '/' Verzeichnis.\n");
+    printf("\t-h, --help\t\tGibt diese Anleitung aus.\n");
+    printf("\t-i, --init\t\tFührt die Initialanweisungen für den ersten Start des Programms aus.\n");
+    printf("\nLICENSE\tGNU General Public License v3.0\n");
+}
+
+
+
+
+
+
+
+
+
 
